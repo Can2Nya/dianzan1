08d42425b2b80981d94110c1a82fe0ed3e54aa9d
can2 <279319529@qq.com>
2018-02-12 14:38:59 +0800

    fix

--- js/index1.js
+++ js/index1.js
@@ -21,22 +21,8 @@ function GetDistance(lat1, lng1, lat2, lng2) {
   return s;
 }
 
+// 获取定位
 var coords, dis;
-// var flag;
-function showJson(lat, lon, dis) {
-  $.getJSON("http://api.map.baidu.com/geocoder/v2/?location=" + lat + "," + lon + "&callback=?&output=json&ak=I2KCehM1RiQrm4rB2QCnwidiTDDh5egh", function (data) {
-      console.log(data.result.addressComponent.city + " " + dis);
-      if (data.result.addressComponent.city){
-
-      }
-          // window.location.href = "addQuest.html" + "?dis=" + dis + "&city=" + encodeURI(data.result.addressComponent.city);
-  });
-
-}
-// var time = 0;
-// var clearLater = 400;
-// var btn;
-// var Tex;
 
 function getLocation() {
   var map = new BMap.Map("allmap");
@@ -49,35 +35,24 @@ function getLocation() {
           coords = r.point;
           dis = GetDistance(r.point.lat, r.point.lng, 26.07, 112.07);//经纬度(维度在前,经度在后)
           dis = Math.round(dis);
-          // alert(dis);
-          // Tex.innerHTML = "还有" + dis + "公里";
-          // btn.innerText = "参与活动";
-          // flag = true;
-          // btn.innerText = "参与活动";
           $(".page-8 i").text(dis);
       }
     }, { enableHighAccuracy: true })
   }catch(e){
     alert(e);
   }
-  
 }
-// function getdis() {
-//   btn = document.getElementById("disBtn");
-//   Tex = document.getElementById("disText");
 
-//   if (btn.innerText == "获取我的位置") {
-//       btn.innerText = "正在获取";
-//       getLocation();
-//   }
-//   else if (btn.innerText == "参与活动") {
-//       if (!flag)
-//           window.location.href = "addQuest.html" + "?dis=" + 0 + "&city=" + encodeURI("位置获取失败");
-//       else {
-//           if (coords.latitude)
-//               showJson(coords.latitude, coords.longitude, dis);
-//           else
-//               showJson(coords.lat, coords.lng, dis);
-//       }
-//   }
-// }
+// 输入信息
+
+$('#next').on('click', function(){
+  if($('#name').val() == '' || $('#tel').val() == ''){
+    alert('请填写信息');
+    return;
+  }
+  if($('#tel').val().replace(/[^0-9]/, '').length !== 11 ){
+    alert('请填写正确的电话');
+    return;
+  }
+  location.href = location.origin + '/share.html';
+})
